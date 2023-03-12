60e455826f1a6a4d87f1e603a5d16dc76d7a89ad
Aniket <sinhaaniket192@gmail.com>
2023-03-12 18:58:22 +0530

    init: react-app

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import { CiWarning,CiMenuFries,CiUser,CiShop } from "react-icons/ci";
+import { HiOutlineShoppingBag } from "react-icons/hi";
+
+import './App.css';
+
+function App() {
+  return (
+   <div>
+      <nav class="main-nav">
+        <div class="container_1">
+          <div class="icon_1"><CiMenuFries size='5rem'/></div>
+        </div>
+
+        <div className="heading">
+            <div><a href="https://www.linkedin.com/company/gnsnalvero/" target="_blank">GNAL</a></div>
+        </div>
+        
+          <li className="Services"><a href="https://www.linkedin.com/company/gnsnalvero/" target="_blank">Services</a></li>
+          <li className="Shop"><a href="https://www.linkedin.com/company/gnsnalvero/" target="_blank">Shop</a></li>
+             <div class="container_2">
+                <div class="icon_2"><CiShop size='2rem'/></div>
+            </div>
+          <div class="container_3">
+                <div class="icon_3"><CiUser size='2rem'/></div>
+          </div>
+       
+      </nav>
+      
+      
+      <body>
+
+        <div class="container">
+          <div class="icon"><CiWarning size='4.5rem'/></div>
+        </div>
+
+        <div className='message'>
+           We’re currently working on this page. We’ll be back with this feature soon. We really acknowledge your support & believe on us.
+        </div>
+        
+        <div className='message_1'>
+          VISIT OTHER SERVICES
+        </div>
+
+      </body>
+
+
+    </div>
+
+
+   
+
+  );
+}
+
+export default App;
