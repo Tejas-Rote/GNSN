c7198a09ff74fac94b445d54b4851387e12faebe
Tejas Rote <tejas292002@gmail.com>
2023-03-20 13:20:29 +0530

    initial

---
+++ src/componenets/Homepage/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import SectionSix from './section/SectionSix'
+
+const homePage = () => {
+    return (
+        <div>Sections
+
+            <div>
+                <SectionSix />
+            </div>
+        </div >
+    )
+}
+
+export default homePage
---
+++ src/componenets/Homepage/section/SectionSix.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import fImage from "../componenet/images/feedback.jpg"
+
+const SectionSix = () => {
+    return (
+        <div>SectionSix
+            <div style={{ backgroundImage: "url(/feedback.jpg)" }}>
+                Image for feedback
+            </div>
+        </div>
+    )
+}
+
+export default SectionSix
